639bda2f0c94065ce14d7fb65a133724722b5a7d
SparkAgness <anabol1984@yandex.ru>
2024-06-23 12:48:05 +0300

    avager switcher

--- mymeteo_st.ino
+++ mymeteo_st.ino
@@ -6,7 +6,7 @@
 #include <WiFiUdp.h>
 
 /* Установите здесь свои SSID и пароль */
-const char* ssid = "Andrey_Fe";  
+const char* ssid = "Andrey_Fe-netis";  
 const char* password = "20031984";  
 
 ESP8266WebServer server(80);
@@ -17,6 +17,8 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org");
 //for pasting bme's measutments into web-interface
 String str_temp;
 String str_press;
+String str_humid;
+String str_average_temp;
 
 String str_time;
 String str_date;
@@ -36,6 +38,7 @@ void PasteAvValue(int begin[4][12], int pastes_row, int paste_place, int vals_ti
 
 int AverageCalc(int* arr_source);
 int AvrPasteCounter(int* arr);
+enum average AvrTypeSwitcher();
 
 void setup() {
     Serial.begin(115200);
@@ -59,8 +62,8 @@ void setup() {
     timeClient.setTimeOffset(10800);
 }
 void loop() {
-    enum avenger {mo_day = 0, mo_night, ye_day, ye_night} avenger_type;
-    int avenger_temp[4][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    enum average {mo_day = 0, mo_night, ye_day, ye_night} average_type, average_typeH;
+    int average_temp[4][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
     timeClient.update();
     str_time = Interpretate(timeClient.getHours(), 1) + ":" + Interpretate(timeClient.getMinutes(), 1);
@@ -69,21 +72,30 @@ void loop() {
     str_date = week_days[timeClient.getDay()] + ",\n " + String(ptm->tm_mday) + " " + months[ptm->tm_mon] + " " + String(ptm->tm_year + 1900);
     str_temp = Interpretate(int(bme.readTemperature()) - 3);
     str_press = Interpretate(int(bme.readPressure())/133);
+    str_humid = Interpretate(int(bme.readHumidity()));
+    str_average_temp = Interpretate(*(*(average_temp + 2)));
     server.handleClient();
 
     if (paste_enable && !ptm->tm_min && !ptm->tm_sec) {
         int hours = ptm->tm_hour;
         if ((hours < 8) || (hours > 19)) {
-        avenger_type = ye_night;
-        if (hours > 19) {hours = hours - 11;}
-    } else if (hours > 7 && hours < 20) {
-            avenger_type = ye_day;
+            average_type = ye_night;
+            if (hours > 19) {hours = hours - 11;}
+        } else if (hours > 7 && hours < 20) {
+            average_type = ye_day;
             hours = hours - 8;
         } 
-        PasteAvValue(avenger_temp, avenger_type, hours, int(bme.readTemperature()));
+        PasteAvValue(average_temp, average_type, hours, int(bme.readTemperature()));
+        average_type = mo_day;
+        average_typeH = ye_day;
+        *(*(average_temp + average_type) + AvrPasteCounter(*(average_temp + average_type))) = AverageCalc(*(average_temp + average_typeH));
+        average_type = mo_night;
+        average_typeH = ye_night;
+        *(*(average_temp + average_type) + AvrPasteCounter(*(average_temp + average_type))) = AverageCalc(*(average_temp + average_typeH));
         paste_enable = false;
     }
     if (!paste_enable && ptm->tm_min) {paste_enable = true;}
+    
 }
 
 void handle_OnConnect() {
@@ -113,6 +125,11 @@ String SendHTML(String str_temp) {
     ptr += "<h3>Pressure is: ";
     ptr += str_press;
     ptr += " mm Hg</h3>\n";
+    ptr += "<h3>The humidity is:";
+    ptr += str_humid;
+    ptr += "%</h3>";
+    ptr += "Average temperature yesterday is: ";
+    ptr += str_average_temp;
     ptr +="</body>\n";
     ptr +="</html>\n";
   return ptr;
@@ -145,6 +162,7 @@ String Interpretate(int val, bool time_format)
     return value = String(hundreds) + String(dec) + String(ones);
 }
 
+//calculating number of fillings arrays days/nights on mo_day/mo_night
 int AvrPasteCounter(int* arr)
 {
     int size = 12, count;
@@ -155,6 +173,7 @@ int AvrPasteCounter(int* arr)
     
 }
 
+//calculating of average value on average_temp array
 int AverageCalc(int* arr_source)
 {
     int count = AvrPasteCounter(arr_source);
@@ -167,3 +186,7 @@ int AverageCalc(int* arr_source)
     }
     return sum;
 }
+enum average AvrTypeSwitcher()
+{
+
+};
