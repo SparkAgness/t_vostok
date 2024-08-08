f72bd7ae656ebfe497b0d8e4853782aa7ea0228e
SparkAgness <anabol1984@yandex.ru>
2024-08-08 05:58:13 +0300

    sectors description begin

--- mymeteo_st/symbol.h
+++ mymeteo_st/symbol.h
@@ -1,6 +1,21 @@
+/*
+Arc sectors sheme:
+
+ 1 | 2
+ -----
+ 3 | 4
+
+ */
+
+
+
+
 #ifndef SYMBOL
 #define SYMBOL
 #define HIGHT 26
+#define VERT_QUARTER 7
+#define HOR_AXIS 14
+#define IN_RAD 4 
 
 #include <Change_Arr.h>
 
@@ -9,11 +24,36 @@ class Symbol final
     private:
         int name_;
         Changeable_Array coord_kit_;
+
+        void Draw_UpSector1(int); //OK
+        void Draw_UpSector2(int);
         void DrawTwo();
 
     public:
         Symbol(int);
         void Symb_Drawing(const int);
+};
+
+void Symbol::Draw_UpSector2(int offset)
+{
+    int out_offset = 7;
+    int tmp[2] {0, 0};
+};
+
+void Symbol::Draw_UpSector1(int offset)
+{
+    int in_offset = 7;
+    int tmp[2] {0, 0};
+    for (int y = 2; y <= VERT_QUARTER; ++y) {
+        switch(y)
+        {
+            case 2: for (int x = offset + in_offset + 4; x <= HOR_AXIS; ++x) {tmp[2] {x, y}; this->coord_kit_.PushBack(tmp); break;}
+            case 3: for (int x = offset + in_offset + 2; x <= HOR_AXIS; ++x) {tmp[2] {x, y}; this->coord_kit_.PushBack(tmp); break;}
+            case 4: for (int x = offset + in_offset + 1; x <= HOR_AXIS - IN_RAD + 1; ++x) {tmp[2]{x,y}; this->coord_kit_.PushBack(tmp); break;} 
+            default: for (int x = offset + in_offset; x <= HOR_AXIS - IN_RAD; ++x) {tmp[2]{x,y}; this->coord_kit_.PushBack(tmp); break;} 
+            
+	}
+    }
 };
 
 void Symbol::Symb_Drawing(const int name)
@@ -25,21 +65,7 @@ Symbol::Symbol (int type) : name_(type), coord_kit_(0, 0) {};
 
 void Symbol::DrawTwo()
 {
-    int offset1 = 7, offset2 = 12;
-    for (int y = 0; y <= HIGHT; ++y) {
-        switch(y) {
-            case 2: 
-                for (x = 0; x < 7; ++x) {int tmp [2] {x + offset1 + 4, y}; this->coord_kit_.PushBack(tmp);} 
-            case 3: 
-                for (x = 0; x < 11; ++x) {int tmp[2] {x + offset1 + 2, y}; this->coord_kit_.PushBack(tmp);}
-            case 4:
-                for (x = 0; x < 14; ++x) {int tmp[2] {x + offset1 + 1, y}; this->coord_kit_.PushBack(tmp);}
-            case 5:
-                for (x = 0; x < 5; ++x) {int tmp[2] {x + offset1, y}; int tmp2[2] {x + offset2, y}; this->coord_kit_.PushBack(tmp); this->coord_kit_.PushBack(tmp2);}
-            case 6: 
-                
-	}
-    }
+
 };
 
 
