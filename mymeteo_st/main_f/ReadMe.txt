c743ae9f40d3b3ff55afb72fdf44bdecf589e61c
Spark Agness <anabol1984@yandex.ru>
2024-10-22 05:06:10 +0300

    in readme

--- mymeteo_st/main_f/ReadMe.txt
+++ mymeteo_st/main_f/ReadMe.txt
@@ -2,6 +2,7 @@ there's memory's leaking without destructors into Symbol_Part inharitancing clas
 Symbol::Get_Private_Part is working, but returns only first member of pair Changeable_Array
 !!!Now we can begin to test cout of figure_kit_ - before this all have been compiled successfully, shared_ptr is ready OK
 The shared pointer is working, but it is wrong with math - coords is wrong OK
-MinMax_Cell is not wrong in Max x, min y - to find deciding ways for searchin minimum... (4th parameter of MinMax_Cell()) it has been debugged
-Erase_Corner is debugging - to test Fill_Erase() - coords_ in SymbolPart is empty 
+
+------------------------------------
+New era - creating of new Changeable_array by linked list in change_arr_.h
 
--- mymeteo_st/main_f/change_arr_.h
+++ mymeteo_st/main_f/change_arr_.h
@@ -1,41 +1,24 @@
 #include <iostream>
-#ifndef CHANGEARR
-#define CHANGEARR
-namespace change_arr {
 
-class Changeable_Array final
+namespace changeable_array
 {
-    private:
-        int* body_;
-	Changeable_Array* left_;
-	Changeable_Array* right_;
-    public:
-	Changeable_Array() : body_{new int[2]} {left_ = nullptr; right_ = nullptr;};
-        Changeable_Array(int x, int y) : body_(new int[2]) {*body_ = x; *(body_ + 1) = y; left_ = nullptr; right_ = nullptr;}; 
-        Changeable_Array(Changeable_Array& node) 
-        {
-            body_ = new int[2] {node.GetBody(false), node.GetBody(true)};
-	    left_ = node.GetNodeLink(true);
-            right_ = node.GetNodeLink(false);
-	};
-        ~Changeable_Array() {delete [] body_;};
-        
-	int GetBody(bool);
-        Changeable_Array* GetNodeLink(bool);
-		
-};
-int Changeable_Array::GetBody(bool first)
-{
-    if (first) return *(body_ + 1);
-    return *body_;
-};
+    class Array final
+    {
+        private:
+	    class ArrNode final
+            {
+                private:
+                    int* coords_;
+		    ArrNode* left_;
+                    ArrNode* right_;
 
-Changeable_Array* Changeable_Array::GetNodeLink(bool left)
-{
-    if (left) return left_;
-    return right_;
-};
+                public:
+                    ArrNode() : coords_{new int [2]} {*coords_ = 0; *(coords_ + 1) = 0; left_ = nullptr; right_ = nullptr;};
+                    ArrNode(int x, int y) : coords_{new int [2]} {*coords_ = x, *(coords_ + 1) = y, left_ = nullptr; right_ = nullptr;};
+                   
+		    ~ArrNode() {delete [] coords_;};
+	    };
+
+        public:
+    };
 }
-
-#endif
-
